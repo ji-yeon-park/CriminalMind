4935cab5a9828246976ae33e64b7a955fd404e3a
kosta <kosta@192.168.0.103>
2019-11-06 20:38:36 +0900

    TEST

--- src/com/kosta/view/MainView.java
+++ src/com/kosta/view/MainView.java
@@ -2,9 +2,8 @@ package com.kosta.view;
 
 public class MainView {
 	public static void main(String[] args) {
- 		System.out.println("짜증나니까 얼른3 되거라");
 
-		//MenuView.menuChoice();
+		MenuView.menuChoice();
 
 	}
 
